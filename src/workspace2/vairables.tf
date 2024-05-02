1b948eba884380ceb2c04086fe5f649aa58b97fb
Cristian Camilo Muñoz <critian.munoz@sofka.com.co>
2024-05-02 15:30:01 -0500

    add-subnet

---
+++ src/workspace2/vairables.tf
@@ -0,0 +1,13 @@
+variable "id_vpc" {
+type = string   
+  
+}
+variable "access_key" {
+  type = string      
+}
+variable "secret_acess_key" {
+    type = string
+}
+variable "env" {
+    type = string
+  }

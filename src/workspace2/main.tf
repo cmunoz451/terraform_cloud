70cce584f8f4037cf67d5010e885038c8e981a17
cmunoz451 <124535948+cmunoz451@users.noreply.github.com>
2024-05-02 15:28:16 -0500

    Update main.tf

--- src/workspace2/main.tf
+++ src/workspace2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "subnet_example" {
-  vpc_id            =  vpc_id
+  vpc_id            =  var.vpc_id
   availability_zone = "us-east-1a"
   cidr_block        = "10.0.1.0/24"
 }

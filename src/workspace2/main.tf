e45840f2121cd12b5be2067fa0cc4f348b9a0b52
Cristian Camilo Muñoz <critian.munoz@sofka.com.co>
2024-05-02 15:36:51 -0500

    add-subnet

--- src/workspace2/main.tf
+++ src/workspace2/main.tf
@@ -1,9 +1,5 @@
 resource "aws_subnet" "subnet_example" {
-<<<<<<< HEAD
   vpc_id            =  var.id_vpc
-=======
-  vpc_id            =  var.vpc_id
->>>>>>> 70cce584f8f4037cf67d5010e885038c8e981a17
   availability_zone = "us-east-1a"
   cidr_block        = "10.0.1.0/24"
 }

8370cca159c21c67786c4f7682441b32bc0c1d04
Cristian Camilo Muñoz <critian.munoz@sofka.com.co>
2024-05-03 16:52:45 -0500

    add-secrets-manager

--- src/workspace2/providers.tf
+++ src/workspace2/providers.tf
@@ -8,13 +8,13 @@
 #   required_version = ">1.3.0"
 # }
 
-# provider "aws" {
-#   region     = "us-east-1"
-#   access_key = var.access_key
-#   secret_key = var.secret_acess_key
-#   default_tags {
-#     tags = {
-#       env = var.env
-#     }
-#   }
-# }
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_acess_key
+  default_tags {
+    tags = {
+      env = var.env
+    }
+  }
+}
--- src/workspace2/vairables.tf
+++ src/workspace2/vairables.tf
@@ -1,9 +1,9 @@
-# variable "access_key" {
-#   type = string      
-# }
-# variable "secret_acess_key" {
-#     type = string
-# }
-# variable "env" {
-#     type = string
-#   }
+variable "access_key" {
+  type = string      
+}
+variable "secret_acess_key" {
+    type = string
+}
+variable "env" {
+    type = string
+  }

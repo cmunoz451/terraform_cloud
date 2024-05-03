686b5e3142ea1ecbb708bea80498ff17fc7d17f6
Cristian Camilo Muñoz <critian.munoz@sofka.com.co>
2024-05-03 12:04:18 -0500

    add-namesubred

--- src/workspace2/main.tf
+++ src/workspace2/main.tf
@@ -2,4 +2,7 @@ resource "aws_subnet" "subnet_example" {
   vpc_id            =  var.id_vpc
   availability_zone = "us-east-1a"
   cidr_block        = "10.0.1.0/24"
+  tags = {
+    Name = "vpc-subred-ccmunoz"  
+  }
 }

5f8f3a55b68d6d04bee87c0544894562f277fc5a
Cristian Camilo Muñoz <critian.munoz@sofka.com.co>
2024-05-03 15:39:20 -0500

    update-data.tf

--- src/workspace2/main.tf
+++ src/workspace2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "subnet_example" {
-  vpc_id            =  data.terraform_remote_state.prueba1.outputs.id.vpc
+  vpc_id            =  data.terraform_remote_state.prueba1.outputs.id_vpc
   availability_zone = "us-east-1a"
   cidr_block        = "10.0.1.0/24"
   tags = {
